8e6e15d6b1ae563a805915b9c0a493cf9c7a9573
Alex Sonneveld <alex.sonneveld@99designs.com>
2018-10-22 10:08:43 +1100

    Fix console log URL output for IPv6

--- server.js
+++ server.js
@@ -18,9 +18,8 @@ const app = express();
 app.use(express.static(__dirname));
 
 const server = app.listen(8081, () => {
+    const host = server.address().address;
+    const port = server.address().port;
 
-  const host = server.address().address;
-  const port = server.address().port;
-
-  console.log('App listening at http://%s:%s', host, port);
+    console.log('App listening at http://%s:%s', host === '::' ? 'localhost' : host, port);
 });
